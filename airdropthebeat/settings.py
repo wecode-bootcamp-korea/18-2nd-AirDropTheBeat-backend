6fdfc9419efb57276c8ffed1863496047a3fc5b5
parkgood1020 <parkgood1020@gmail.com>
2021-03-30 20:56:06 +0900

    ADD : user, room, book model.py add complete

--- airdropthebeat/settings.py
+++ airdropthebeat/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'user',
+    'book',
+    'room'
 ]
 
 MIDDLEWARE = [
---
+++ book/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+class BookStatus(models.Model):
+    name          = models.CharField(max_length=45, null=True)
+    
+    class Meta:
+        db_table = "book_status"
+
+class Book(models.Model):
+    room             = models.ForeignKey('room.Room', on_delete=models.CASCADE)
+    user             = models.ForeignKey('user.User', on_delete=models.CASCADE)
+    book_status      = models.ForeignKey('BookStatus', on_delete=models.SET_NULL, null=True)
+    created_at       = models.DateTimeField(auto_now_add=True)
+    number_of_guests = models.PositiveIntegerField()
+    message          = models.CharField(max_length=45, null=True)
+    start_date       = models.DateTimeField()
+    end_date         = models.DateTimeField()
+    serial_number    = models.CharField(max_length=45)
+    
+    class Meta:
+        db_table = "books"
+
