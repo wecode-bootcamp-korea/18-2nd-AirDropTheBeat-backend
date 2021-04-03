2012b5689296dc4936fea2f452e204e707a8e95c
sehyeong <sehyeong@sehyeongui-MacBookAir.local>
2021-04-03 23:55:51 +0900

    Modify: tests.py 수정

--- user/tests.py
+++ user/tests.py
@@ -1,4 +1,6 @@
-import json
+import json, bcrypt
+
+from mock import patch, MagicMock
 
 from django.test import TestCase, Client
 
@@ -115,3 +117,170 @@ class SignUpTest(TestCase):
         response = client.post('/user/signup', json.dumps(data), content_type='application/json')
 
         self.assertEqual(response.status_code, 400)
+    
+    
+class SignInTest(TestCase):
+    def setUp(self):
+        hashed_password = bcrypt.hashpw("123456789".encode('utf-8'), bcrypt.gensalt())
+        db_password     = hashed_password.decode('utf-8')
+
+        User.objects.create(first_name    = "sehyeong",
+                            last_name     = "kim",
+                            date_of_birth = "19961210",
+                            email         = "sshkim96@gmail.com",
+                            password      = db_password,
+                            phone_number  = "01099264524",
+                            )
+
+    def tearDown(self):
+        User.objects.all().delete()
+
+    def test_signin_user_success(self): 
+        data = {
+                "email":"sshkim96@gmail.com",
+                "password":"123456789"
+                }
+
+        response = client.post('/user/signin', json.dumps(data), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
+    def test_signin_user_does_not_exist(self):
+        data = {
+                "email":"wecode@gmail.com",
+                "password":"1234567"
+                }
+
+        response = client.post('/user/signin', json.dumps(data), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+
+    def test_signin_key_error(self):
+        data = {
+                "email":"sshkim96@gmail.com"
+                }
+
+        response = client.post('/user/signin', json.dumps(data), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+
+    def test_signin_jsondecode_error(self):
+        data = {}
+
+        response = client.post('/user/signin', json.dumps(data), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+
+
+class KakaoSignInTest(TestCase):
+    def setUp(self):
+        User.objects.create(
+                            first_name = "세형",
+                            last_name  = "김",
+                            email      = "sshkim96@gmail.com",
+                            password   = "1234567"
+                            )
+
+        User.objects.create(
+                            email    = "tkddus614@naver.com",
+                            kakao_id = 3784375
+                            )
+
+    def tearDown(self):
+        User.objects.all().delete()
+
+    @patch('user.views.requests')
+    def test_kakao_signin_create_user_success(self, mocked_request):
+
+        class MockedResponse:
+            def json(self):
+                return {
+                        "id": 547834,
+                        "kakao_account": {
+                            "email": "haha@gmail.com"
+                            }
+                        }
+
+        mocked_request.get = MagicMock(return_value = MockedResponse())
+        headers  = {'HTTP_Authorization': 'fake_token'}
+        response = client.get('/user/signin-kakao', content_type='application/json', **headers)
+
+        self.assertEqual(response.status_code, 200)
+
+    @patch('user.views.requests')
+    def test_kakao_signin_get_user_success(self, mocked_request):
+
+        class MockedResponse:
+            def json(self):
+                return {
+                        "id": 3784375,
+                        "kakao_account": {
+                            "email": "tkddus614@naver.com"
+                            }
+                        }
+        mocked_request.get = MagicMock(return_value = MockedResponse())
+        headers  = {'HTTP_Authorization': 'fake_token'}
+        response = client.get('/user/signin-kakao', content_type='application/json', **headers)
+
+        self.assertEqual(response.status_code, 200)
+
+    @patch('user.views.requests')
+    def test_kakao_signin_email_already_exists(self, mocked_request):
+
+        class MockedResponse:
+            def json(self):
+                return {
+                        "id":3423423,
+                        "kakao_account": {
+                            "email": "sshkim96@gmail.com"
+                            }
+                        }
+
+        mocked_request.get = MagicMock(return_value = MockedResponse())
+        headers  = {'HTTP_Authorization': 'fake_token'}
+        response = client.get('/user/signin-kakao', content_type='application/json', **headers)
+        
+        message = response.json()['message']
+        self.assertEqual(message, 'USER_EMAIL_ALREADY_EXISTS')
+
+    @patch('user.views.requests')
+    def test_kakao_signin_invalid_kakao_token(self, mocked_request):
+
+        class MockedResponse:
+            def json(self):
+                return {
+                        "code": -401
+                        }
+
+        mocked_request.get = MagicMock(return_value=MockedResponse())
+        headers  = {'HTTP_Authorization': 'fake_token'}
+        response = client.get('/user/signin-kakao', content_type='application/json', **headers)
+
+        message = response.json()['message']
+        self.assertEqual(message, 'INVALID_TOKEN')
+
+    @patch('user.views.requests')
+    def test_kakao_sign_in_key_error(self, mocked_request):
+        
+        class MockedResponse:
+            def json(self):
+                return {
+                        }
+
+        mocked_request.get = MagicMock(return_value=MockedResponse())
+        headers  = {'HTTP_Authorization': 'fake_token'}
+        response = client.get('/user/signin-kakao', content_type='application/json', **headers)
+
+        message = response.json()['message']
+        self.assertEqual(message, 'KEYERROR')
+
+    @patch('user.views.requests')
+    def test_token_does_not_exist(self, mocked_request):
+        
+        class MockedResponse:
+            def json(self):
+                return {
+                        "id": 123
+                        }
+
+        mocked_request.get = MagicMock(return_value=MockedResponse())
+        response = client.get('/user/signin-kakao', content_type='application/json')
+        
+        message = response.json()['message']
+        self.assertEqual(message, 'TOKEN_DOES_NOT_EXIST')
--- user/views.py
+++ user/views.py
@@ -1,4 +1,4 @@
-import json, bcrypt, re, requests
+import json, bcrypt, re, requests, jwt
 from django.db.models.query_utils import Q
 
 from django.views import View
@@ -6,7 +6,7 @@ from django.http  import JsonResponse
 from json.decoder import JSONDecodeError
 
 from user.models import User
-from my_settings import REGEX_NAME, REGEX_DATE_OF_BIRTH, REGEX_EMAIL, REGEX_PASSWORD, REGEX_PHONE_NUMBER
+from my_settings import REGEX_NAME, REGEX_DATE_OF_BIRTH, REGEX_EMAIL, REGEX_PASSWORD, REGEX_PHONE_NUMBER, SECRET_KEY, ALGORITHM
 
 class SignUpView(View):
     def post(self, request):
@@ -48,3 +48,58 @@ class SignUpView(View):
             return JsonResponse({'message':'KEY ERROR'}, status=400)
         except JSONDecodeError:
             return JsonResponse({'message':'JSON DECODE ERROR'}, status=400)
+
+
+class SignInView(View):
+    def post(self, request):
+        try:
+            data     = json.loads(request.body)
+            email    = data['email']
+            password = data['password']
+
+            exist_user = User.objects.filter(email=email).exists()
+            if not exist_user:
+                return JsonResponse({'message':'INVALID_USER'}, status=400)
+
+            user           = User.objects.get(email=email)
+            check_password = bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8'))
+            if not check_password:
+                return JsonResponse({'message':'INVALID_USER'}, status=400)
+
+            token = jwt.encode({'user_id':user.id}, SECRET_KEY, algorithm=ALGORITHM)
+            return JsonResponse({'message':'SUCCESS', 'token':token}, status=200)
+
+        except KeyError:
+            return JsonResponse({'message':'KeyError'}, status=400)
+        
+        except json.decoder.JSONDecodeError:
+            return JsonResponse({'message':'JSONDecodeError'}, status=400)
+
+
+class KakaoSignInView(View):
+    def get(self, request):
+        try:
+            token = request.headers.get('Authorization')
+            if not token:
+                return JsonResponse({'message':'TOKEN_DOES_NOT_EXIST'}, status=400)
+
+            KAKAO_API_SERVER = "https://kapi.kakao.com/v2/user/me"
+            response         = requests.get(KAKAO_API_SERVER, headers={"Authorization": f"Bearer {token}"})
+            account          = response.json()
+            
+            if account.get('code') == -401:
+                return JsonResponse({'message':'INVALID_TOKEN'}, status=400)
+
+            kakao_id    = int(account['id'])
+            kakao_email = account['kakao_account']['email']
+
+            if User.objects.filter(email=kakao_email, kakao_id=None).exists():
+                return JsonResponse({'message': 'USER_EMAIL_ALREADY_EXISTS'}, status=400)
+
+            user, created = User.objects.get_or_create(kakao_id=kakao_id, email=kakao_email)
+
+            access_token  = jwt.encode({'user_id':user.id}, SECRET_KEY, algorithm=ALGORITHM)
+            return JsonResponse({'message':'SUCCESS', 'access_token':access_token}, status=200)
+
+        except KeyError:
+            return JsonResponse({'message':'KEYERROR'}, status=400)
