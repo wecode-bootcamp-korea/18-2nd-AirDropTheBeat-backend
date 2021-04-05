06d11feeb4a90e3e4d922508078bd782d289afd9
sehyeong <sehyeong@sehyeongui-MacBookAir.local>
2021-04-05 16:28:58 +0900

    Modify: 최종수정 - RoomDetailView, ReviewView 쿼리 최적화

--- room/tests.py
+++ room/tests.py
@@ -1,3 +1,156 @@
-from django.test import TestCase
+from django.test import TestCase,Client
 
-# Create your tests here.
+from user.models import User, Host
+from room.models import Room, Type, CheckinType, City, District, AddressLine, Review
+
+client = Client()
+class RoomDetailViewTest(TestCase):
+    def setUp(self):
+        user = User.objects.create(
+                                    first_name = "sehyeong",
+                                    last_name  = "kim",
+                                    email      = "sshkim96@gmail.com",
+                                    password   = "12345678"
+                                    )
+
+        room = Room.objects.create(
+                                    title          = "서울집",
+                                    type           = Type.objects.create(name        = "집전체", 
+                                                                         description = "집전체를 사용하게 됩니다."),
+                                    host           = Host.objects.create(user           = user,
+                                                                         residence      = "서울시 강남구",
+                                                                         language       = "한국어, 영어", 
+                                                                         available_hour = "12:00~18:00"),
+                                    checkin        = CheckinType.objects.create(name        = "셀프 체크인", 
+                                                                                description = "셀프 체크인으로 편하게 이용하세요."),
+                                    city           = City.objects.create(name="서울시"),
+                                    district       = District.objects.create(city = City.objects.create(name = "서울시"),
+                                                                             name = "강남구"),
+                                    address_line   = AddressLine.objects.create(city     = City.objects.create(name = "서울시"), 
+                                                                                district = District.objects.create(city = City.objects.create(name = "서울시"),
+                                                                                                                   name = "강남구"), 
+                                                                                name     = "역삼동"),
+                                    zip_code       = "12346",
+                                    description    = "좋은 집",
+                                    price          = 140000,
+                                    discount_rate  = 0,
+                                    latitude       = 37.5047692,
+                                    longitude      = 127.0062895,
+                                    maximum_people = 2,
+                                    checkin_time   = "12:00:00",
+                                    checkout_time  = "16:00:00",
+                                    bedroom        = 1,
+                                    bathroom       = 1,
+                                    bed            = 1
+                                    )
+
+    def tearDown(self):
+        User.objects.all().delete()
+        Host.objects.all().delete()
+        Room.objects.all().delete()
+        City.objects.all().delete()
+        Type.objects.all().delete()
+        CheckinType.objects.all().delete()
+        District.objects.all().delete()
+        AddressLine.objects.all().delete()
+
+    def test_get_room_information_success(self):
+        response = client.get('/room/2?check_in=2021-04-15&check_out=2021-04-16&adults=1&child=1', content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
+    def test_room_id_does_not_exist(self):
+        response = client.get('/room/200', content_type='application/json')
+        self.assertEqual(response.status_code, 404)
+
+
+class ReviewViewTest(TestCase):
+    def setUp(self):
+        user = User.objects.create(
+                                    first_name = "sehyeong",
+                                    last_name  = "kim",
+                                    email      = "sshkim96@gmail.com",
+                                    password   = "12345678"
+                                    )
+
+        room = Room.objects.create(
+                                    title          = "서울집",
+                                    type           = Type.objects.create(name        = "집전체", 
+                                                                         description = "집전체를 사용하게 됩니다."),
+                                    host           = Host.objects.create(user           = user,
+                                                                         residence      = "서울시 강남구",
+                                                                         language       = "한국어, 영어", 
+                                                                         available_hour = "12:00~18:00"),
+                                    checkin        = CheckinType.objects.create(name        = "셀프 체크인", 
+                                                                                description = "셀프 체크인으로 편하게 이용하세요."),
+                                    city           = City.objects.create(name="서울시"),
+                                    district       = District.objects.create(city = City.objects.create(name="서울시"),
+                                                                             name = "강남구"),
+                                    address_line   = AddressLine.objects.create(city     = City.objects.create(name="서울시"), 
+                                                                                district = District.objects.create(city = City.objects.create(name="서울시"),
+                                                                                                                   name = "강남구"), 
+                                                                                name     = "역삼동"),
+                                    zip_code       = "12346",
+                                    description    = "좋은 집",
+                                    price          = 140000,
+                                    discount_rate  = 0,
+                                    latitude       = 37.5047692,
+                                    longitude      = 127.0062895,
+                                    maximum_people = 2,
+                                    checkin_time   = "12:00:00",
+                                    checkout_time  = "16:00:00",
+                                    bedroom        = 1,
+                                    bathroom       = 1,
+                                    bed            = 1
+                                    )
+        
+        Review.objects.create(
+                              user          = User.objects.create(first_name="sehyeong", last_name="kim"),
+                              room          = room,
+                              content       = "좋아요",
+                              cleanliness   = 5,
+                              accuracy      = 3,
+                              communication = 3,
+                              location      = 2,
+                              checkin       = 5,
+                              satisfaction  = 4
+                              )
+
+        Review.objects.create(
+                              user          = User.objects.create(first_name="raon", last_name="kim"),
+                              room          = room,
+                              content       = "강아지가 좋아하는 숙소",
+                              cleanliness   = 3,
+                              accuracy      = 5,
+                              communication = 4,
+                              location      = 5,
+                              checkin       = 3,
+                              satisfaction  = 3
+                              )
+        
+        Review.objects.create(
+                              user          = User.objects.create(first_name="code", last_name="we"),
+                              room          = room,
+                              content       = "위코드",
+                              cleanliness   = 4,
+                              accuracy      = 2,
+                              communication = 5,
+                              location      = 5,
+                              checkin       = 4,
+                              satisfaction  = 3
+                              )
+
+    def tearDown(self):
+        User.objects.all().delete()
+        Host.objects.all().delete()
+        Room.objects.all().delete()
+        City.objects.all().delete()
+        Type.objects.all().delete()
+        CheckinType.objects.all().delete()
+        District.objects.all().delete()
+        AddressLine.objects.all().delete()
+        Review.objects.all().delete()
+
+    def test_get_reviews_success(self):
+        response = client.get('/room/1/review',content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
--- room/views.py
+++ room/views.py
@@ -1,3 +1,97 @@
-from django.shortcuts import render
+from django.views     import View
+from django.http      import JsonResponse
+from django.db.models import Avg
 
-# Create your views here.
+from room.models import Room, Image, RoomConvenience, RoomRule, Review
+from user.models import User, Host
+
+class RoomDetailView(View):
+    def get(self, request, room_id):
+        checkin_date  = request.GET.get('check_in', None)
+        checkout_date = request.GET.get('check_out', None)
+        adults        = request.GET.get('adults', 0) 
+        child         = request.GET.get('child', 0) 
+        baby          = request.GET.get('baby', 0) 
+
+        exist_room = Room.objects.filter(id=room_id).exists()
+        if not exist_room:
+            return JsonResponse({'message': 'ROOM_DOES_NOT_EXIST'}, status=404)
+
+        room = Room.objects.select_related('city', 'district', 'address_line','type', 'checkin', 'host').prefetch_related('image_set', 'roomconvenience_set', 'roomrule_set').get(id=room_id)
+
+        result = {
+                "checkin_date": checkin_date,
+                "checkout_date": checkout_date,
+                "adult": adults,
+                "child": child,
+                "baby": baby,
+                "number_of_guests": int(adults)+int(child)+int(baby),
+                "price": room.price,
+                "discount_rate": room.discount_rate,
+                "title": room.title,
+                "city": room.city.name,
+                "district": room.district.name,
+                "address_line": room.address_line.name,
+                "images_url": [image.image_url for image in room.image_set.all()],
+                "type": room.type.name,
+                "type_description": room.type.description,
+                "maximum_people": room.maximum_people,
+                "bedroom": room.bedroom,
+                "bathroom": room.bathroom,
+                "bed": room.bed,
+                "checkin_type": room.checkin.name,
+                "checkin_type_description": room.checkin.description,
+                "description": room.description,
+                "conveniences": [room.convenience.name for room in room.roomconvenience_set.all()],
+                "lat": room.latitude,
+                "lng": room.longitude,
+                "checkin_time": room.checkin_time,
+                "checkout_time": room.checkout_time,
+                "room_rules": [rule.rule.name for rule in room.roomrule_set.all()],
+                "host_id": room.host.id,
+                "host_name": room.host.user.first_name,
+                "host_image":room.host.user.image_url,
+                "host_language": room.host.language,
+                "host_available_hour": room.host.available_hour
+                }
+
+        return JsonResponse(result, status=200)
+        
+class ReviewView(View):
+    def get(self, request, room_id):
+        reviews = Review.objects.filter(room_id=room_id).select_related('user')
+        
+        if reviews.exists():
+            cleanliness   = reviews.aggregate(Avg('cleanliness'))['cleanliness__avg']
+            accuracy      = reviews.aggregate(Avg('accuracy'))['accuracy__avg']
+            communication = reviews.aggregate(Avg('communication'))['communication__avg']
+            location      = reviews.aggregate(Avg('location'))['location__avg']
+            checkin       = reviews.aggregate(Avg('checkin'))['checkin__avg']
+            satisfaction  = reviews.aggregate(Avg('satisfaction'))['satisfaction__avg']
+            total_average = (cleanliness + accuracy + communication + location + checkin + satisfaction)/6
+            counts        = reviews.count()
+
+            detail_reviews = [{
+                                "id": review.id,
+                                "user_id": review.user.id,
+                                "user_name": review.user.first_name,
+                                "user_image": review.user.image_url if review.user.image_url else [],
+                                "created_at": review.created_at,
+                                "content": review.content
+                                } for review in reviews]
+
+            result = { 
+                    "total_average": round(total_average,2),
+                    "counts": counts,
+                    "cleanliness": round(cleanliness,1),
+                    "accuracy": round(accuracy,1),
+                    "communication": round(communication,1),
+                    "location": round(location,1),
+                    "checkin": round(checkin,1),
+                    "satisfaction": round(satisfaction,1),
+                    "detail_reviews": detail_reviews
+                    }
+        else:
+            result = {}
+
+        return JsonResponse(result, status=200)
